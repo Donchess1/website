fed1ba0c38d55a7f5e6994fd54b952027c2df581
Donchess1 <stepabod@.com>
2024-07-31 11:23:35 +0000

    adjusted manage.py

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 set -o errexit
 
 # Install dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
+#pip install --upgrade pip
+#pip install -r requirements.txt
 
 # Run migrations
 python manage.py migrate
