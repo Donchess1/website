8e9bdf8e4e846e6f97454bee0266fc60e0877313
Donchess1 <stepabod@.com>
2024-07-31 17:32:30 +0000

    merged apps into grillz

--- grillz/urls.py
+++ grillz/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('menu/', views.timetable, name='menu'),
     path('review/', views.review, name='review'),
     path('signup/', views.signup, name='signup'),
+    path('shop/', views.shop, name='shop'),
+    path('add/', views.add_product, name='add_product'),
+    path('cart/', views.cart_detail, name='cart_detail'),
+    path('product_list/', views.product_list, name='product_list'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
 ]
