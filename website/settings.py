bb35540feca3d4db51183bad6417ba7b59ba05ed
Donchess1 <stepabod@.com>
2024-07-31 11:07:56 +0000

    127.0.0.1 added to host

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nsd4ej4z%j-0je6uh9nx=c*2@(t25lkby5w6w(c(=$rsw3$^hu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel,app', '.now.sh' 'localhost']
+ALLOWED_HOSTS = ['.vercel,app', '.now.sh' 'localhost', '127.0.0.1']
 
 
 # Application definition
